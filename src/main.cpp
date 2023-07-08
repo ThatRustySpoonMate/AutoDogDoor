9f4017712b8b51931fdde60d9b9437e3157be0af
Ethan <ethanhharris5@gmail.com>
2023-07-08 23:21:39 +1000

    Added lockout switch and relay support

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 
+// Define private macros
+#define RELAY_PIN 4 // Pin for relay
+#define LOCKOUT_SWITCH_PIN 17 // Pin for switch
+#define LOCKOUT_SWITCH_LOCKED 0 // Switch is closed
+#define LOCKOUT_SWITCH_UNLOCKED 1 // Switch is open
+
 // Define Global Vars
 int SCAN_DURATION = 2; //In seconds
 int16_t RSSI_THRESHOLD = -80; // Threshold for RSSI value to determine if device is in range
@@ -14,7 +20,9 @@ uint32_t SCAN_INTERVAL = 50; // Time between scans in ms
 int previousRSSI = 0;
 int currentRSSI = 0;
 int RSSI_INC_THRESHOLD = 5; // Threshold for RSSI increase to determine if door should be opened
-int RSSI_DOOR_OVERRIDE = -75; // Threshold for RSSI increase to determine if door should be opened
+int RSSI_DOOR_OVERRIDE = -70; // Threshold for RSSI to determine if door should be opened
+
+uint8_t door_status = 0x00; // 0x00 = closed, 0x01 = open
 
 // Flags when ellie beacon is found
 bool pinged = false;
@@ -33,6 +41,8 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 
 // Declare Functions
 void unlock_door();
+void lock_door();
+//void IRAM_ATTR button_pressed();
 
 
 // put function declarations here:
@@ -42,17 +52,32 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Ellie Door running.");
 
+  // Set up relay pin
+  pinMode(RELAY_PIN, OUTPUT);
+  // Set up button pin
+  pinMode(LOCKOUT_SWITCH_PIN, INPUT_PULLUP);
+
+  //attachInterrupt(BUTTON_PIN, button_pressed, HIGH);
+
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan(); //create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+  setCpuFrequencyMhz(80);
+  Serial.print("CPU: "); Serial.print(getCpuFrequencyMhz()); Serial.println("MHz");
+  Serial.print("APB: "); Serial.print(getApbFrequency()); Serial.println("Hz");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   BLEScanResults foundDevices = pBLEScan->start(SCAN_DURATION, false);
+
+  if(digitalRead(LOCKOUT_SWITCH_PIN) == LOCKOUT_SWITCH_LOCKED) {
+    lock_door();
+  }
   
   if(pinged) { // Ellie beacon was located
     Serial.print("Previous RSSI: "); Serial.println(previousRSSI);
@@ -60,10 +85,14 @@ void loop() {
 
     if(currentRSSI > RSSI_DOOR_OVERRIDE) {
       Serial.println("Ellie is close enough to open the door!");
-      // unlock_door() 
+      if(digitalRead(LOCKOUT_SWITCH_PIN) == LOCKOUT_SWITCH_UNLOCKED) {
+        unlock_door();
+      } 
     } else if(previousRSSI != 0 and currentRSSI - previousRSSI > RSSI_INC_THRESHOLD) {
       Serial.println("Ellie is getting closer!");
-      // unlock_door() 
+      if(digitalRead(LOCKOUT_SWITCH_PIN) == LOCKOUT_SWITCH_UNLOCKED) {
+        unlock_door();
+      }
     } else if(previousRSSI != 0 and currentRSSI - previousRSSI < 0) {
       Serial.println("Ellie is getting further away!");
     } else if(previousRSSI != 0){
@@ -82,11 +111,39 @@ void loop() {
 
   pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
   delay(SCAN_INTERVAL);
+
+  
 }
 
 
 void unlock_door(){
+  
   Serial.println("Unlocking door!");
+  
+  //digitalWrite(RELAY_PIN, HIGH);
+  //delay(500);
+  //digitalWrite(RELAY_PIN, LOW);
+
+  /*
+  if(door_status == 0x00) {
+    // Open door
+    Serial.println("Unlocking door!");
+    door_status = 0x01;
+  } */
+
+  // Set timer for 10 seconds ?
+  
+  return;
+}
+
+void lock_door() {
+  Serial.println("Locking door!");
 
   return;
 }
+
+/** Interrupt approach
+void IRAM_ATTR button_pressed() {
+  Serial.println("Button Pressed!");
+} 
+*/
